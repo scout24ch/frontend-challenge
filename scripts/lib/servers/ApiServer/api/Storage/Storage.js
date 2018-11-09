2f6538b4ad125d22ae3c8f662ead6166b0650522
Matthias Jäggli <matthias.jaeggli@gmail.com>
2018-11-09 17:56:44 +0100

    add crud operations

--- scripts/lib/servers/ApiServer/api/Storage/Storage.js
+++ scripts/lib/servers/ApiServer/api/Storage/Storage.js
@@ -1,13 +1,57 @@
 const path = require('path')
 const Store = require('data-store')
 
+const META_KEY = '__meta'
+
 class Storage extends Store {
   constructor (name) {
     super({
       name,
-      path: path.join(__dirname, `${name}.json`),
+      path: path.join(__dirname, 'stores', `${name}.json`),
       debounce: 10
     })
+
+    if (!this.has(META_KEY)) {
+      this.set(META_KEY, {
+        lastId: 0
+      })
+    }
+  }
+  add (value) {
+    const meta = this.get(META_KEY)
+    meta.lastId++
+
+    const id = meta.lastId.toString()
+    const now = new Date().toISOString()
+    const data = {
+      ...value,
+      id,
+      created: now,
+      modified: now
+    }
+
+    this.set(id, data)
+    this.set(META_KEY, meta)
+    return data
+  }
+  update (id, value) {
+    id = id.toString()
+    const modified = new Date().toISOString()
+    const old = this.get(id)
+    const updated = {
+      ...old,
+      ...value,
+      id,
+      created: old.created,
+      modified
+    }
+    this.set(id, updated)
+    return updated
+  }
+  list () {
+    return Object.keys(this.data)
+      .filter(key => key !== META_KEY && this.has(key))
+      .map(id => this.get(id))
   }
 }
 
--- scripts/lib/servers/ApiServer/api/controllers/recipes.js
+++ scripts/lib/servers/ApiServer/api/controllers/recipes.js
@@ -6,39 +6,89 @@ const list = (req, res) => {
   const { page: pageParam, pageSize: pageSizeParam, search: searchParam } = req.swagger.params
   const { value: page } = pageParam
   const { value: pageSize } = pageSizeParam
-  const { value: search } = searchParam
-  res.json({
-    page, pageSize, search
-  })
+  let { value: search } = searchParam
+
+  const start = (page - 1) * pageSize
+  search = search && search.trim() || ''
+  const searches = search.split(/\s+/)
+  const list = store.list()
+    .filter(item => {
+      if (!searches.length) {
+        return true
+      }
+
+      let include = false
+      const text = [
+        item.title,
+        item.description,
+        item.ingredients.reduce((acc, item) => acc + ' ' + item, '')
+      ].join(' ')
+
+      searches.forEach(search => {
+        include = include || text.includes(search)
+      })
+
+      return include
+    })
+    .slice(start, start + pageSize)
+
+  res.json(list)
 }
 
 const get = (req, res) => {
   const { id: idParam} = req.swagger.params
-  const { value: id } = idParam
+  const id = idParam.value.toString()
 
-  console.log(store.has(id))
+  if (!store.has(id)) {
+    return res.status(404).json({
+      message: `${id} not found`
+    })
+  }
 
-  res.json({
-    id
-  })
+  res.json(store.get(id))
+}
+
+const del = (req, res) => {
+  const { id: idParam} = req.swagger.params
+  const id = idParam.value.toString()
+
+  if (!store.has(id)) {
+    return res.status(404).json({
+      message: `${id} not found`
+    })
+  }
+
+  res.json(store.get(id))
+}
+
+const update = (req, res) => {
+  const { id: idParam} = req.swagger.params
+  const id = idParam.value.toString()
+
+  const { body: bodyParam} = req.swagger.params
+  const { value } = bodyParam
+
+  if (!store.has(id)) {
+    return res.status(404).json({
+      message: `${id} not found`
+    })
+  }
+
+  res.json(store.update(id, value))
 }
 
 const add = async (req, res) => {
   const { body: bodyParam} = req.swagger.params
-  const { value, valid, error } = bodyParam
-
-  console.log(value, valid, error)
-
-  if (!valid) {
-    // throw new Error(error)
-  }
-
-
-  res.end()
+  const { value } = bodyParam
+  res
+    .status(201)
+    .json(store.add(value))
 }
 
 module.exports = {
   add,
+  delete: del,
+  get,
   list,
-  get
+  update
 }
