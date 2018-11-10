442591367dae25b5bf73eee1b99ee37cd7188f93
Matthias Jäggli <matthias.jaeggli@gmail.com>
2018-11-10 18:47:03 +0100

    add api docs link

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -1,13 +1,14 @@
 (function () {
+  // wait for dom ready and call the api
   document.addEventListener('DOMContentLoaded', function () {
-    // wait for dom ready and call the api
     fetch('http://localhost:8081/healthcheck')
       .then(function (result) {
         return result.json()
       })
       .then(function (json) {
         // replace the status text
-        document.querySelector('.frontend-challenge-demo-box .status').textContent = json.message
+        document.querySelector('.frontend-challenge-demo-box .status').textContent = json.message + ','
+        document.querySelector('.frontend-challenge-demo-box .docs-link').style.display = 'inline'
       })
   })
 })()
